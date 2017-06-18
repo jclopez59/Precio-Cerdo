115cb0dd54d6a75e074c5bc3aeed9d23cf991c55
Juan Carlos López Mesa <jclopez59@gmail.com>
2017-06-18 08:29:05 -0500

    Add files via upload

---
+++ server.R
@@ -0,0 +1,55 @@
+library(shiny)
+library(chron)
+library(ggplot2)
+
+precio <- read.csv("Precio.csv", header = T)
+precio$Fecha <- as.Date(precio$Fecha, "%d/%m/%Y")
+attach(precio)
+
+
+shinyServer(function(input, output) {
+  
+  PrecioF <- reactive(precio [Fecha >= input$FFecha[1] & Fecha <= input$FFecha[2] &
+                              Region == input$Region, ])
+  PrecioG <- reactive(precio [Fecha >= input$FFecha[1] & Fecha <= input$FFecha[2], ])
+  Ant <- reactive(precio [Fecha >= input$FFecha[1] & Fecha <= input$FFecha[2] &
+                            Region == levels(Region)[1], c(1,4)])
+  Bog <- reactive(precio [Fecha >= input$FFecha[1] & Fecha <= input$FFecha[2] &
+                            Region == levels(Region)[2], 4])
+  Caribe <- reactive(precio [Fecha >= input$FFecha[1] & Fecha <= input$FFecha[2] &
+                               Region == levels(Region)[3], 4])
+  Eje <- reactive(precio [Fecha >= input$FFecha[1] & Fecha <= input$FFecha[2] &
+                            Region == levels(Region)[4], 4])
+  Valle <- reactive(precio [Fecha >= input$FFecha[1] & Fecha <= input$FFecha[2] &
+                              Region == levels(Region)[5], 4])
+  
+  
+  output$General <- renderTable(PrecioF() [,c(1, 4)])
+  
+  output$GrafxReg <- renderPlot({plot(PrecioF()[,2], PrecioF()[,4], pch = 16,
+                                      main = paste("Precio Semanal", input$Region),
+                                      xlab = "Fecha", col = "darkblue",
+                                      ylab = "Precio Kg en Pie ($)", lwd = 2,
+                                      panel.first =  grid(), type = "b")})
+  
+  output$Var <- renderPlot({barplot(diff(PrecioF()[,4]),
+                                    main = "Variaci'on del Precio Respecto a la Semana Anterior",
+                                    xlab = "Fecha", ylab = "Variaci'on Precio", 
+                                    col = ifelse(diff(PrecioF()[,4]) < 0, 
+                                                 "darkred", "darkgreen"))
+    
+    output$Variacion <- renderTable({data.frame(Semana = PrecioF()[-1,1],
+                                                Var = diff(PrecioF()[,4]))})
+    
+    output$Resumen <- renderTable(data.frame(Ant(), Bog(), Caribe(), Eje(), Valle())
+      
+    )
+   
+    output$GrafREs <- renderPlot(qplot(Fecha, Precio, data=PrecioG(), shape = Region,
+                                       color=Region, geom = c("point", "line"),
+                                       main = "Precio Semanal", xlab = "Fecha",
+                                       ylab = "Precio Kg en Pie"))
+    
+      })
+})
+
---
+++ ui.R
@@ -0,0 +1,75 @@
+library(shiny)
+library(ggplot2)
+
+precio <- read.csv("Precio.csv", header = T)
+precio$Fecha <- as.Date(precio$Fecha, "%d/%m/%Y")
+attach(precio)
+
+shinyUI(fluidPage(
+  
+
+  titlePanel("Precio Semanal del Cerdo en Pie"),
+  
+
+  sidebarLayout(
+    sidebarPanel(
+      dateRangeInput("FFecha", "Fechas", start = (max(precio$Fecha)-63), 
+                     end = max(precio$Fecha), format = "dd-mm-yyyy", 
+                     language = "es", min = min(precio$Fecha), 
+                     max = max(precio$Fecha)),
+      
+      selectInput("Region", "Region", choices = levels(Region)),
+      hr(),
+      br(),
+      h6("Desarrollado por:"),
+      tags$img(src = "ControlG.png", height = 100, width = 100),
+      h6("jclopez5@unal.edu.co"),
+      h6("dagc32@gmail.com")
+      ),
+    
+
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Regi'on", 
+                 plotOutput("GrafxReg"),
+                 tableOutput("General"),
+                 h4("Fuente: Porkcolombia, Ronda de Precios."),
+                 h6("https://asociados.porkcolombia.co/porcicultores/index.php?option=com_porcicultores&view=cifras&Itemid=104&ronda=2017")
+        ),
+        
+        tabPanel("Variaci'on Precio",
+                 plotOutput("Var"),
+                 tableOutput("Variacion"),
+                 h4("Fuente: Porkcolombia, Ronda de Precios."),
+                 h6("https://asociados.porkcolombia.co/porcicultores/index.php?option=com_porcicultores&view=cifras&Itemid=104&ronda=2017")
+        ),
+        
+        tabPanel("Resumen",
+                 plotOutput("GrafREs"),
+                 tableOutput("Resumen"),
+                 h4("Fuente: Porkcolombia, Ronda de Precios."),
+                 h6("https://asociados.porkcolombia.co/porcicultores/index.php?option=com_porcicultores&view=cifras&Itemid=104&ronda=2017")
+        ),
+        
+       tabPanel("A Cerca de",
+                tags$img(src = "ControlG.png", height = 200, width = 200,
+                                 align = "center"),
+                h3("Soluciones Estad'isticas y Tecnol'ogicas"),
+                hr(),
+                h5("Juan C. L'opez M."),
+                h6("Zootecnista - Especialista en Estad'istica"),
+                h6("jclopez5@unal.edu.co - 300 322 3901"),
+                br(),
+                h5("Diego Giraldo"),
+                h6("Administrador Agropecuario"),
+                h6("dagc32@gmail.com - 321 817 7447"),
+                br(),
+                h5("Este aplicativo sirve como fuente de consulta interactiva de los 
+                   precios del cerdo en pie publicados por Porkcolombia semanalmente.
+                   La informaci'on mostrada corresponde a datos disponibles en la 
+                   pagina web https://asociados.porkcolombia.co/porcicultores/
+                   index.php?option=com_porcicultores&view=cifras&Itemid=104&ronda=2017"))
+        )
+    )
+  )
+))
